1439316b48156ac1509b80ddc1b9fdde21d7688b
Victoria Beauray Sagady <cephypi@pm.me>
2025-11-12 13:23:01 -0600

    docs: Added xml doc too class header

--- DCS-SR-Client/Input/BindingManager.cs
+++ DCS-SR-Client/Input/BindingManager.cs
@@ -1,6 +1,12 @@
 namespace Ciribob.DCS.SimpleRadio.Standalone.Client.Input;
 
+/// <summary>
+/// Manager that handles binding and changing inputs, reading and writing etc.
+/// </summary>
 public class BindingManager
 {
-    
+    BindingManager()
+    {
+        
+    }
 }
