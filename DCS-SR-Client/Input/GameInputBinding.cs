03f8e7a1f709147bbd04e476d851495add90a4c4
Victoria Beauray Sagady <cephypi@pm.me>
2025-11-12 09:33:28 -0600

    fix: renamed file and added IEquatable and ops

---
+++ DCS-SR-Client/Input/GameInputBinding.cs
@@ -0,0 +1,82 @@
+using System;
+using Ciribob.DCS.SimpleRadio.Standalone.Common.Settings;
+
+namespace Ciribob.DCS.SimpleRadio.Standalone.Client.Input;
+
+/// <summary>
+/// Binding Record 
+/// </summary>
+public class GameInputBinding : IEquatable<GameInputBinding>
+{
+    /// <summary>
+    /// Command that is triggered
+    /// </summary>
+    public InputBinding Binding { get; set; }
+    
+    /// <summary>
+    /// Primary combination of inputs
+    /// </summary>
+    public InputTrigger Input { get; set; }
+    
+    /// <summary>
+    /// Secondary combination of inputs
+    /// </summary>
+    public InputTrigger Modifier { get; set; }
+
+    /// <summary>
+    /// Initialize Generic Binding, JSON Friendly
+    /// </summary>
+    public GameInputBinding()
+    {
+    }
+
+    public bool Equals(GameInputBinding other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return Binding == other.Binding;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return Equals((GameInputBinding)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return (int)Binding;
+    }
+    
+    public static bool  operator ==(GameInputBinding left, GameInputBinding right)
+    {
+        return Equals(left, right);
+    }
+    
+    public static bool operator !=(GameInputBinding left, GameInputBinding right)
+    {
+        return !Equals(left, right);
+    }
+}
